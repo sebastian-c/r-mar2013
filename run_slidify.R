023d8d72a8c7c95b00f92057967f33b81eeee074
Sebastian <sebastian.campbell@sydney.edu.au>
2013-03-04 15:15:18 +1100

    Added bibliography. Removed reshape2 error and implemented it as text.

--- run_slidify.R
+++ run_slidify.R
@@ -3,6 +3,7 @@
 ## dev_mode()  #Turn on devmode if you don't want to affect your library
 # install_github("slidify", "ramnathv")
 # install_github("slidifyLibraries", "ramnathv")
+# install_github("knitcitations", "cboettig")
 
 library(slidify)
 
